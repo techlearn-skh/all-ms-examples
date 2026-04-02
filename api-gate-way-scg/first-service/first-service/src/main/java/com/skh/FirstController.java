a22c52912810984573cf437099667758fbe5e208
techlearn-skh <techlearn508@gmail.com>
2026-04-02 18:33:16 +0530

    new changes

--- api-gate-way-scg/first-service/first-service/src/main/java/com/skh/FirstController.java
+++ api-gate-way-scg/first-service/first-service/src/main/java/com/skh/FirstController.java
@@ -2,9 +2,12 @@ package com.skh;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 import java.time.LocalDateTime;
 
@@ -13,10 +16,26 @@ import java.time.LocalDateTime;
 public class FirstController {
     private static final Logger log = LoggerFactory.getLogger(FirstController.class);
 
+    @Autowired
+    private WebClient.Builder webClientBuilder;
+
+
     @GetMapping("/message")
     public String test() {
-        String message = String.format("%s() method called in %s!!!, time %s",Thread.currentThread().getStackTrace()[1].getMethodName(), this.getClass().getName(), LocalDateTime.now());
+        String message = String.format("%s() method called in %s!!!, time %s", Thread.currentThread().getStackTrace()[1].getMethodName(), this.getClass().getName(), LocalDateTime.now());
         log.info(message);
         return message;
     }
+
+    @GetMapping("/call-fs-to-ss")
+    public Mono<String> callFromFirstServiceToSecondService() {
+        return webClientBuilder.build()
+                .get()
+                .uri("http://SECOND-SERVICE/ss/message")
+                .retrieve()
+                .bodyToMono(String.class)
+                .doOnSubscribe(sub -> log.info("Calling SECOND-SERVICE"))
+                .doOnSuccess(res -> log.info("Response: " + res));
+    }
+
 }
--- api-gate-way-scg/first-service/first-service/src/main/java/com/skh/FirstServiceApplication.java
+++ api-gate-way-scg/first-service/first-service/src/main/java/com/skh/FirstServiceApplication.java
@@ -1,13 +1,31 @@
 package com.skh;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class FirstServiceApplication {
+
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(FirstServiceApplication.class, args);
+	}
+
+	@Configuration
+	public class WebClientConfig {
+		@Bean
+		@LoadBalanced
+		public WebClient.Builder webClientBuilder() {
+			return WebClient.builder();
+		}
 	}
 
 }
