21f318e9debec227ed6a1f57befabac48d5d3e3e
techlearn-skh <techlearn508@gmail.com>
2026-09-14 11:50:16 +0530

    new changes

--- api-gate-way-scg/api-gate-way-scg/api-gate-way-scg/src/test/java/com/skh/ApiGateWayScgApplicationTests.java
+++ api-gate-way-scg/api-gate-way-scg/api-gate-way-scg/src/test/java/com/skh/ApiGateWayScgApplicationTests.java
@@ -3,11 +3,11 @@ package com.skh;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+/*@SpringBootTest
 class ApiGateWayScgApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
 
-}
+}*/
